99344228b035f560cb5a51c567c54a6038196e15
Michael <ssjbudgie@gmail.com>
2023-07-15 17:11:41 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import Webcam from "react-webcam";
+import "./App.css";
+import {drawRect} from "./utilities"; 
+
+
+const App = () => {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+
+  const runCoco = async () => {
+    const net = await tf.loadGraphModel('https://directionstfod.s3.us-east-2.amazonaws.com/model/model.json')
+    
+    setInterval(() => {
+      detect(net);
+    }, 16.7)
+  }
+
+
+
+  const detect = async (net) => {
+
+    if (
+      typeof webcamRef.current != "undefined" &&
+      webcamRef.current != null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+
+      const img = tf.browser.fromPixels(video);
+      const resized = tf.image.resizeBilinear(img, [640, 480]);
+      const casted = resized.cast('int32');
+      const expanded = casted.expandDims(0);
+      const obj = await net.executeAsync(expanded);
+
+      const boxes  = await obj[5].array();
+      const classes = await obj[6].array();
+      const scores = await obj[7].array();
+
+      const ctx = canvasRef.current.getContext("2d");
+
+     
+
+      requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.2, videoWidth, videoHeight, ctx)}); 
+
+      tf.dispose(img)
+      tf.dispose(resized)
+      tf.dispose(casted)
+      tf.dispose(expanded)
+      tf.dispose(obj) 
+    }
+
+  }
+
+  useEffect(()=>{runCoco()},[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          muted={true} 
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
